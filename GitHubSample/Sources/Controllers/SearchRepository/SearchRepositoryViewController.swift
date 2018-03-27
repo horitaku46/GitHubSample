79ec7e955083c07d97eabe2ebee6cb763efab369
horitaku46 <horitaku46@gmail.com>
2018-03-27 13:09:45 +0900

    Ready Sxample

---
+++ GitHubSample/Sources/Controllers/SearchRepository/SearchRepositoryViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SearchRepositoryViewController.swift
+//  GitHubSample
+//
+//  Created by Takuma Horiuchi on 2018/03/27.
+//  Copyright © 2018年 Takuma Horiuchi. All rights reserved.
+//
+
+import UIKit
+
+final class SearchRepositoryViewController: UIViewController {
+}
