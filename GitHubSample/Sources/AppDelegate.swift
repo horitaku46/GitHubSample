636d58de42311c1180f8e615334e55f80959a3c3
horitaku46 <horitaku46@gmail.com>
2018-05-07 14:49:37 +0900

    Add progressBar to web

--- GitHubSample/Sources/AppDelegate.swift
+++ GitHubSample/Sources/AppDelegate.swift
@@ -8,39 +8,23 @@
 
 import UIKit
 
+let isPhoneX: Bool = {
+    guard #available(iOS 11.0, *),
+        UIDevice().userInterfaceIdiom == .phone else {
+            return false
+    }
+    let nativeSize = UIScreen.main.nativeBounds.size
+    let (w, h) = (nativeSize.width, nativeSize.height)
+    let (d1, d2): (CGFloat, CGFloat) = (1125.0, 2436.0)
+    return (w == d1 && h == d2) || (w == d2 && h == d1)
+}()
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
--- GitHubSample/Sources/Controllers/Web/WebViewController.swift
+++ GitHubSample/Sources/Controllers/Web/WebViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import WebKit
 
 final class WebViewController: UIViewController, Storyboardable {
+
+    private enum Const {
+        static let progressViewScaleY: CGFloat = isPhoneX ? 16.5 : 7.5
+        static let progressViewTopConstant: CGFloat = isPhoneX ? 20.8 : 8.8
+    }
 
     class func make(urlStr: String) -> WebViewController {
         let viewController = WebViewController.makeFromStoryboard()
@@ -17,4 +23,47 @@ final class WebViewController: UIViewController, Storyboardable {
     }
 
     private var urlStr: String!
+    private var webView: WKWebView = {
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.ignoresViewportScaleLimits = true
+        let webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
+    private var progressView: UIProgressView = {
+        let progressView = UIProgressView(progressViewStyle: .bar)
+        progressView.transform = CGAffineTransform(scaleX: 1, y: Const.progressViewScaleY)
+        progressView.progress = 0.5
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        return progressView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(webView)
+        webView.addSubview(progressView)
+        if #available(iOS 11.0, *) {
+            NSLayoutConstraint.activate([
+                webView.topAnchor.constraint(equalTo: view.topAnchor),
+                webView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                webView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                progressView.topAnchor.constraint(equalTo: webView.topAnchor, constant: Const.progressViewTopConstant),
+                progressView.leadingAnchor.constraint(equalTo: webView.leadingAnchor),
+                progressView.trailingAnchor.constraint(equalTo: webView.trailingAnchor)
+                ])
+        } else {
+            NSLayoutConstraint.activate([
+                webView.topAnchor.constraint(equalTo: view.topAnchor),
+                webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                progressView.topAnchor.constraint(equalTo: webView.topAnchor, constant: Const.progressViewTopConstant),
+                progressView.leadingAnchor.constraint(equalTo: webView.leadingAnchor),
+                progressView.trailingAnchor.constraint(equalTo: webView.trailingAnchor),
+                ])
+        }
+        webView.load(URLRequest(url: URL(string: urlStr)!))
+    }
 }
